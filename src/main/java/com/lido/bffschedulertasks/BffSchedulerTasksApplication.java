7c9ca680ef1b09a653fa7724a0549463dbfb64ac
Lidoan <lidoanpinto@gmail.com>
2025-05-13 17:07:16 -0300

    Commit initial

---
+++ src/main/java/com/lido/bffschedulertasks/BffSchedulerTasksApplication.java
@@ -0,0 +1,13 @@
+package com.lido.bffschedulertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSchedulerTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffSchedulerTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lido/bffschedulertasks/BffSchedulerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lido.bffschedulertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSchedulerTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
